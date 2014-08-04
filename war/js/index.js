0048082e85c24ad018414d462ea900f89ed15062
Ashutosh Singh <dh_ashu@hotmail.com>
2014-08-04 12:17:25 +0530

    Test Commint

--- war/js/index.js
+++ war/js/index.js
@@ -1,28 +1,28 @@
 $(document).ready(function(){
 	
-	//function to work on design text
-	$('.designBtn').click(function(){
-		var url = 'resources/Design.jsp';
-		$.get(
-				url,
-				function(responseText){
-					$('#mainContent').html(responseText);
-				}
-		);
-		
-	});	
-		
-	$('.priceBtn').click(function(){
-		var url = 'resources/Pricing.jsp';
-		$.get(
-				url,
-				function(responseText){
-					$('#mainContent').html(responseText);
-				}
-		);
-		
-	});	
-	
+//	//function to work on design text
+//	$('.designBtn').click(function(){
+//		var url = 'resources/Design.jsp';
+//		$.get(
+//				url,
+//				function(responseText){
+//					$('#mainContent').html(responseText);
+//				}
+//		);
+//		
+//	});	
+//		
+//	$('.priceBtn').click(function(){
+//		var url = 'resources/Pricing.jsp';
+//		$.get(
+//				url,
+//				function(responseText){
+//					$('#mainContent').html(responseText);
+//				}
+//		);
+//		
+//	});	
+//	
 });
 	
 	
